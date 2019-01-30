fd17fd5cc6ce47e3785e3abf819d5f34dd3b4195
root <root@ip-172-31-28-156.ap-southeast-1.compute.internal>
2019-01-30 09:35:21 +0000

    create redis-slaves

--- redis-master.sh
+++
@@ -1,5 +0,0 @@
- #!/bin/bash
-mode="protected-mode no"
-sed -i '/protected-mode yes/c\'"$mode"  /home/ubuntu/redis-5.0.3/redis.conf
-cd /home/ubuntu/redis-5.0.3/src
-./redis-server --protected-mode no  
